68cb146869eeeeb3d767235b53b7195175140a2b
riyuzenn <riyuzenn@gmail.com>
2022-09-12 10:03:30 +0200

    ♻️  ref: update file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import React from "react";
+import { ThemeProvider } from "next-themes";
 
-function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+function riyu({ Component, pageProps }: AppProps) {
+    return (
+        <React.Fragment>
+            <ThemeProvider attribute="class" defaultTheme="dark">
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </React.Fragment>
+    );
 }
 
-export default MyApp;
+export default riyu;
