2c8a9a3373638e8a21dc5ad7245830d97bbf19c9
ryuu <66673988+riyuzenn@users.noreply.github.com>
2023-07-20 14:10:22 +0800

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,24 +10,24 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    <!-- Primary Meta Tags -->
-<title>@riyuzenn — home</title>
+
+                    <title>@riyuzenn — home</title>
 <meta name="title" content="@riyuzenn — home" />
 <meta name="description" content="average unmotivated dev" />
 
-<!-- Open Graph / Facebook -->
 <meta property="og:type" content="website" />
 <meta property="og:url" content="https://riyuzenn.tech" />
 <meta property="og:title" content="@riyuzenn — home" />
 <meta property="og:description" content="average unmotivated dev" />
 <meta property="og:image" content="https://n.riyuzenn.tech/preview.png" />
 
-<!-- Twitter -->
 <meta property="twitter:card" content="summary_large_image" />
 <meta property="twitter:url" content="https://riyuzenn.tech" />
 <meta property="twitter:title" content="@riyuzenn — home" />
 <meta property="twitter:description" content="average unmotivated dev" />
 <meta property="twitter:image" content="https://n.riyuzenn.tech/preview.png" />
+
+                    
                     <link
                         href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap"
                         rel="stylesheet"
